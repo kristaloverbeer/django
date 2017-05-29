78d7e8858ff004adcc91bcb2153a16e6152b7595
ebris <ebris@presta.aldebaran.com>
2017-05-29 17:49:20 +0200

    first very basic blog app with django

---
+++ beginning_django/django_project/blog/views.py
@@ -0,0 +1,13 @@
+import datetime
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello Django")
+
+
+def today_is(request):
+    now = datetime.datetime.now()
+    html = "<html><body>Current date and time: {0}</body></html>".format(now)
+    return HttpResponse(html)
